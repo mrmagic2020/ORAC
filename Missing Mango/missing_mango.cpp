134d8c9f9456fa32141941f7e50ce420df4bef29
mrmagic2020 <2732298611@qq.com>
2023-06-06 15:46:39 +1000

    add new solution: missing_mango

---
+++ Missing Mango/missing_mango.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+  int Ix, Cx, Id, Cd, Io[2], Co[2], loc;
+  ifstream inputFile("manin.txt");
+  inputFile >> Ix >> Cx >> Id >> Cd;
+  cout << Ix << " " << Cx << " " << Id << " " << Cd << endl;
+  inputFile.close();
+  Io[0] = Ix - Id;
+  Io[1] = Ix + Id;
+  Co[0] = Cx - Cd;
+  Co[1] = Cx + Cd;
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
+      cout << Io[i] << " " << Co[j] << endl;
+      if (Io[i] == Co[j])
+        loc = Io[i];
+    }
+  }
+  ofstream outputFile("manout.txt");
+  outputFile << loc;
+  outputFile.close();
+  return 0;
+}
