bdc95756a2563f39e36928880295d25752eea9a7
mrmagic2020 <2732298611@qq.com>
2023-06-10 08:27:44 +1000

    add new solution

---
+++ Python/Street Construction/street_construction.py
@@ -0,0 +1,12 @@
+# PASSED
+
+from math import ceil
+
+inputFile = open("streetin.txt", "r")
+outputFile = open("streetout.txt", "w")
+
+n, k = [int(x) for x in list((inputFile.read().split(" ")))]
+
+ans = ceil((n - k) / (k + 1))
+
+outputFile.write(str(ans))
