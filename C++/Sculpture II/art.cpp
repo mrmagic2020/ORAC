be033e2f78a1d65674935a00bb5623beab89576d
Arnold Zhou <magicarnold2022@gmail.com>
2024-10-31 09:34:34 +1100

    chore: update files

--- C++/Sculpture II/art.cpp
+++ C++/Sculpture II/art.cpp
@@ -1,33 +1,31 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <stack>
 
 using namespace std;
-const int MAXN = 1e6 + 6;
 
-int N, psum[2 * MAXN], heights[2 * MAXN];
+int N, ans;
 
 int main()
 {
     ifstream in("artin.txt");
+    ofstream out("artout.txt");
     in >> N;
-    int maxHeight = 0, maxWidth = 0;
-    for (int i = 1, t, w, h; i <= N; i++)
+    /**
+     * pair<left_pos, height>
+     */
+    stack<pair<int, int>> blocks;
+    for (int i = 0, t, w, h; i < N; i++)
     {
         in >> t >> w >> h;
-        psum[t] += h;
-        psum[t + w + 1] -= h;
-        maxWidth = max(maxWidth, t + w + 1);
+        while (blocks.size() && blocks.top().first < t) blocks.pop();
+        if (blocks.size()) h += blocks.top().second;
+        blocks.push({t + w - 1, h});
+        ans = max(ans, h);
     }
+    out << ans << endl;
     in.close();
-
-    for (int i = 1; i <= maxWidth; i++)
-    {
-        heights[i] = heights[i - 1] + psum[i];
-        maxHeight = max(maxHeight, heights[i]);
-    }
-    ofstream out("artout.txt");
-    out << maxHeight << endl;
     out.close();
     return 0;
 }
